bae658e9c55463dfb03a70d2c631b37f70b7eb7a
Александр Шушаков <a.shushakov@gnzs.ru>
2025-12-01 18:09:53 +0800

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
-import dayjs from 'dayjs';
-import timezone from 'dayjs/plugin/timezone';
-import utc from 'dayjs/plugin/utc';
+import * as dayjs from 'dayjs';
+import * as timezone from 'dayjs/plugin/timezone';
+import * as utc from 'dayjs/plugin/utc';
 
 import { AppModule } from './app.module';
 
